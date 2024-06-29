f2f1c37b096115cb8935a0819f41243cda79dc0d
Daniel Rojas <danielrojas243@gmail.com>
2024-06-28 21:27:04 -0500

    update

--- template-home.php
+++ template-home.php
@@ -7,6 +7,7 @@ get_header();
 
   <main>
     <section class="sectionHome">
+      <img class="animation" src="<?php echo IMG_BASE . 'animation.gif'?>" alt="">
         <div class="sectionHome__button">
             <a class="button button--white" href="/welcome">WELCOME</a>
         </div>
